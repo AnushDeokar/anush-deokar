c3d549be88dab4dd8862c6aa70e3b1406b40ac3a
anushdeokar <deokaranush@gmail.com>
2024-08-13 22:23:00 +0530

    configure eslint prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"
-import { ToggleTheme } from "@/components/ui/toggle-theme";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css"
+
+import { ToggleTheme } from "@/components/ui/toggle-theme"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Anush Deokar",
   description: "Portfolio website created by Nextjs",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-        {children}
-        <div className="fixed left-4 bottom-4">
-          <ToggleTheme />
-        </div>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <div className="fixed bottom-4 left-4">
+            <ToggleTheme />
+          </div>
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
